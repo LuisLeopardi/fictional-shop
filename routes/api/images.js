4cbefcc8d7fc4366e290450ab820c93f9f327cde
Luis Leopardi <supernano1998@gmail.com>
2020-09-25 15:09:17 -0400

    commit

--- routes/api/images.js
+++ routes/api/images.js
@@ -3,9 +3,9 @@ const {Battery, Phone, Accessory} = require('../../models/item');
 
 router.get('/', async (req,res)=>{
     
-    const batteryArray = await  Battery.find({}).limit(5)
-    const accessoryArray = await Accessory.find({}).limit(5)
-    const phoneArray = await Phone.find({}).limit(5)
+    const batteryArray = await  Battery.find().limit(5)
+    const accessoryArray = await Accessory.find().limit(5)
+    const phoneArray = await Phone.find().limit(5)
 
     const batteryData = batteryArray.map(e=>{
         const batteryStructure = {
